e017efb85201b24ca97ad08b464cc5d909ce8405
Abhishek041100 <gabhishek2000@gmail.com>
2022-10-26 17:55:47 -0300

    working module	modified:   src/app/app-routing.module.ts working file	modified:   src/app/app.component.css working file	modified:   src/app/app.component.html working file	modified:   src/app/app.component.spec.ts working file	modified:   src/app/app.component.ts working file	modified:   src/app/app.module.ts Changed file structure	deleted:    src/app/coffeeList/coffeeList.component.css changed file structure	deleted:    src/app/coffeeList/coffeeList.component.html chnaged file structure	deleted:    src/app/coffeeList/coffeeList.component.ts changed file structure	deleted:    src/app/coffeeList/coffeeList.module.ts working file	modified:   src/app/services/coffee.service.ts changed file structure	deleted:    src/app/store/actions.ts changed file structure	deleted:    src/app/store/effects.ts changed file structure	deleted:    src/app/store/reducers.ts chnaged file structure	deleted:    src/app/store/selectors.ts chnaged file structure	deleted:    src/app/types/appState.interface.ts changed file structure	deleted:    src/app/types/coffee.interface.ts chnaged file structure	deleted:    src/app/types/coffeeState.interface.ts chnaged file structure	src/app/coffees/ chnaged file structure	src/app/models/ chnaged file structure	src/app/store/
    !wq

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./coffees/coffee.module').then((m) => m.CoffeesModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,20 @@
-import { state } from '@angular/animations';
-import { Component, OnInit} from '@angular/core';
-//import {HttpClient} from '@angular/common/http';
-import { CoffeeService } from './services/coffee.service';
+import { getLoading } from './store/Shared/shared.selector';
+import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-//import {CoffeeInterface} from 'src/app/types/coffee.interface'
+import { AppState } from './store/app.state';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-//  coffees: CoffeeInterface[] = []
+  title = 'ngrx-counter';
+  showLoading: Observable<boolean>;
+  constructor(private store: Store<AppState>) {}
 
-  constructor(private item: Store, coffeeService: CoffeeService){}
-  productList = undefined;
-  coffeeAdd = this.item.subscribe((state) => {this.productList=state;
-    }
-  )
-  ngOnInit(): void{
-    this.item.dispatch({ type: '[List of Products/API] Get Coffee Success'});
-    //this.http.get("https://random-data-api.com/api/coffee/random_coffee")
-    //this.coffeeService.getCoffee().subscribe(
-    //  res => {console.log('res', res)
-    //})
+  ngOnInit() {
+    this.showLoading = this.store.select(getLoading);
   }
 }
